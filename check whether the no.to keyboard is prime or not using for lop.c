4a5e4e6c24e84f3ac32c730bbbc0309f96107994
apekshawangane <wanganeapekasha@gmail.com>
2025-06-07 12:05:19 +0530

    Add files via upload

---
+++ check whether the no.to keyboard is prime or not using for lop.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int number,i rem 
+    printf ("Enter the number \t");
+    scanf ("%d",& num);
+    for (i = 2; i < num; i++)
+    {
+       rem = num % i;
+       if (rem = = 0)
+       {
+          check =1
+       }
+    }
+           if ( check = = 0)
+           printf ("prime");
+        }
+        else
+        {
+            printf (" NOT prime")
+        }
+        returen 0;
+          
+}
