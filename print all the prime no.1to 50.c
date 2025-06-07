f1a352ed04583dd102d8a62a2ce3d60d9d72c1c2
apekshawangane <wanganeapekasha@gmail.com>
2025-06-07 12:05:57 +0530

    Add files via upload

---
+++ print all the prime no.1to 50.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j,flag,rem;
+    for (i =1;i < = 50; i ++)
+    {
+    flag=0;
+    for (i=2; i < i,i++)
+    {
+    rem = i%i ;
+    if (rem = = 0)
+        {
+         flag = 1; 
+        }
+    }
+    if (flag = = 0)
+      {
+        printf("%d," i);
+      }
+    }
+     return 0 ;  
+}
