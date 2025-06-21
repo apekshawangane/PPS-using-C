3f53ba87ff6d5f4a7629163ed50327c26a04c366
apekshawangane <wanganeapekasha@gmail.com>
2025-06-21 08:31:11 +0530

    Add files via upload

---
+++ print series no..c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main()
+{
+    printf ("Enter the number :");
+    scanf("%d",& n);
+    for ( int i= 0; i< = n; i++)
+    { 
+         printf ("%d", i);
+    }   
+         printf("\n");
+         return 0 ;
+}
