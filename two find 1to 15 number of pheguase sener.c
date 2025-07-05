69d41644a04ec1db4b88b45a6100694ada818ef2
apekshawangane <wanganeapekasha@gmail.com>
2025-07-05 22:33:12 +0530

    Add files via upload

---
+++ two find 1to 15 number of pheguase sener.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+int main ()
+{
+     int i, pn = 1, cn , nn;
+     for(i = 1;< = 15; i ++)
+     {
+         pn = cn;
+         cn = nn;
+         nn = pn + cn
+         printf ("%d \n", nn);
+     }
+     return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
